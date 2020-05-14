98da7da3d5a9b9aa07ac2f36a2d738b4b39fc9ca
lijiawei <986809553@qq.com>
2020-05-14 15:31:14 +0800

    test1

--- src/main/java/com/jenkins/demo/Test.java
+++ src/main/java/com/jenkins/demo/Test.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @RequestMapping("/hello")
     public String test(){
-        return "hello world";
+        return "hello worlds";
     }
 }
